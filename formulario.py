f9d40914e35dbb605c09cc2c8f32ec91627ad86a
Replit user <>
2023-12-06 22:30:36 +0000

    feat: add organizing priting

--- formulario.py
+++ formulario.py
@@ -3,28 +3,41 @@ def cadastrar_usuario(informacoes):
   arquivo.write(informacoes)
   arquivo.close()
 
+
 def ler_usuarios():
 
   arquivo = open("formulario.txt", 'r', encoding='utf-8')
   usuarios = arquivo.read().split("\n")
 
-  print("\nUsuários cadastrados:\n")
-  for usuario in usuarios:
-     print(usuario)
+  print("\nUSUÁRIOS CADASTRADOS:\n")
+  for usuario in usuarios[0:-1]:
+    nome, idade, sexo, telefone = usuario.split("|")
+    if sexo == "M":
+      sexo = "Masculino"
+    else:
+      sexo = "Feminino"
+
+    print("Nome:", nome)
+    print("Sexo", sexo)
+    print("Idade:", idade)
+    print("Telefone", telefone + "\n")
+
   arquivo.close()
+
 
 def main():
   while True:
-      nome = input("Nome:")
-      if nome == '0': 
-          break
-      idade = input("Idade:")
-      sexo = input("Sexo (M ou F):")
-      telefone = input("Telefone:")
-      informacoes = nome + '|' + idade + "|" + sexo + "|" + telefone + "\n"
-      cadastrar_usuario(informacoes)
+    nome = input("Nome:")
+    if nome == '0':
+      break
+    idade = input("Idade:")
+    sexo = input("Sexo (M ou F):")
+    telefone = input("Telefone:")
+    informacoes = nome + '|' + idade + "|" + sexo + "|" + telefone + "\n"
+    cadastrar_usuario(informacoes)
 
   ler_usuarios()
+
 
 if __name__ == "__main__":
   main()
